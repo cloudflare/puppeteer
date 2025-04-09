f4f87996d1662969bbd161f12267ceb4836b4f26
Rui Figueira <rui.figueira@gmail.com>
2025-04-09 10:51:31 +0100

    fix: exposes __name as closure variable in browser evaluated functions (#98)
    Port of https://github.com/cloudflare/playwright/pull/25
    
    Fixes: #96

---
+++ test-workers/src/tests/evaluate.test.ts
@@ -0,0 +1,14 @@
+import {fetch} from 'undici';
+import {describe, it} from 'vitest';
+
+describe('browser evaluates', () => {
+  it('evaluates function with inner function', async ({expect}) => {
+    await new Promise(resolve => {
+      setTimeout(resolve, 30000);
+    });
+    const response = await fetch(
+      `https://test-workers.cloudflare-browser-rendering-085.workers.dev/evaluate`
+    );
+    expect(await response.text()).toMatchSnapshot();
+  });
+});
